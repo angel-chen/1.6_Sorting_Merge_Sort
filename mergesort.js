466efd285ad0277dc6aa8abfab14cb011482bbd0
marisaoppenheim <marisa.oppenheim@yahoo.com>
2018-04-23 15:24:11 -0400

    split and merge

--- mergesort.js
+++ mergesort.js
@@ -13,7 +13,29 @@ function split(wholeArray) {
 
 function merge(arr1, arr2) {
   let mergedArray = [];
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] > arr2[i]) {
+        mergedArray.push(arr2[i], arr1[i]);
+      } else {
+        mergedArray.push(arr1[i], arr2[i]);
+      }
+    }
+    
+  } else {
+      for (let i = 0; i < arr2.length; i++) {
+        if (arr1[i] === undefined) {
+          mergedArray.push(arr2[i]);
+        }
+        else if (arr1[i] > arr2[i]) {
+          mergedArray.push(arr2[i], arr1[i]);
+        } else {
+          mergedArray.push(arr1[i], arr2[i]);
+      }
 
-  
+    }
+  }
+
+  return mergedArray;
 
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,15 +1,19 @@
 describe('Split Array function', function() {
   let arr = [5, 3, 2, 1, 6, 7];
-  let arr1 = [1, 4, 8];
-  let arr2 = [2, 3, 6];
-
   it('is able to split an array into two halves', function() {
     expect(split(arr)).toEqual([[5, 3, 2], [1, 6, 7]]);
   });
 });
 
 describe('Merge function', function(){
+  let arr1 = [1, 4, 8];
+  let arr2 = [2, 3, 6];
+  let arr3 = [1, 4, 8, 9];
   it('is able to merge two sorted arrays into one sorted array', function(){
     expect(merge(arr1, arr2)).toEqual([1, 2, 3, 4, 6, 8]);
   });
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect(merge(arr2, arr3)).toEqual([1, 2, 3, 4, 6, 8, 9]);
+  });
 });
+
